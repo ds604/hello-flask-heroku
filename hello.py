eeefc98423b9b06e6c466968866e03e1a806c1be
David Sarma <davidsarma@gmail.com>
2022-01-04 20:37:02 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import numpu as np
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/numpy')
+def randMatrix():
+    return np.random.rand(5,5)
 
 if __name__ == '__main__':
     app.run()
